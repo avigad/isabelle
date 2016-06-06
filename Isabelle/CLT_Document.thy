d923d0826fcef826e404733ed170e4942121aa84
Johannes Hölzl <hoelzl@in.tum.de>
2016-06-06 17:48:15 +0200

    add snippets for all TODOs

--- Isabelle/CLT_Document.thy
+++ Isabelle/CLT_Document.thy
@@ -6,34 +6,6 @@ begin
 
 declare [[show_brackets = false, show_question_marks = false]]
 
-text \<open>
- \DefineSnippet{tendstoadd}{
-   @{thm tendsto_add}
- }%EndSnippet
-\<close>
-
-text_raw \<open>\DefineSnippet{skorohod}{\<close>
-theorem Skorohod:
-  fixes \<mu> :: "nat \<Rightarrow> real measure"
-    and M :: "real measure"
-  assumes \<mu>: "\<And>n. real_distribution (\<mu> n)"
-  assumes M: "real_distribution M"
-  assumes \<mu>_to_M: "weak_conv_m \<mu> M"
-  shows "\<exists>(\<Omega> :: real measure) (Y_seq :: nat \<Rightarrow> real \<Rightarrow> real) (Y :: real \<Rightarrow> real).
-    prob_space \<Omega> \<and>
-    (\<forall>n. Y_seq n \<in> measurable \<Omega> borel) \<and>
-    (\<forall>n. distr \<Omega> borel (Y_seq n) = \<mu> n) \<and>
-    Y \<in> measurable \<Omega> lborel \<and>
-    distr \<Omega> borel Y = M \<and>
-    (\<forall>x \<in> space \<Omega>. (\<lambda>n. Y_seq n x) \<longlonglongrightarrow> Y x)"
-text_raw \<open>}%EndSnippet\<close>
-oops
-
-text_raw \<open>\DefineSnippet{countableatoms}{\<close>
-lemma countable_atoms: "countable {x. measure M {x} > 0}"
-text_raw \<open>}%EndSnippet\<close>
-oops
-
 text_raw \<open>\DefineSnippet{cltproof}{\<close>
 theorem (in prob_space) central_limit_theorem:
   fixes X :: "nat \<Rightarrow> 'a \<Rightarrow> real"
@@ -143,6 +115,35 @@ proof -
 qed
 text_raw \<open>}%EndSnippet\<close>
 
+text_raw \<open>\DefineSnippet{tendstoadd}{\<close>
+lemma tendsto_add:
+  fixes f g :: "_ \<Rightarrow> 'a::topological_monoid_add"
+  assumes "(f \<longlongrightarrow> a) F" and "(g \<longlongrightarrow> b) F"
+  shows "((\<lambda>x. f x + g x) \<longlongrightarrow> a + b) F"
+text_raw \<open>}%EndSnippet\<close>
+oops
+
+text_raw \<open>\DefineSnippet{skorohod}{\<close>
+theorem Skorohod:
+  fixes \<mu> :: "nat \<Rightarrow> real measure" and M :: "real measure"
+  assumes "\<And>n. real_distribution (\<mu> n)" "real_distribution M"
+    and "weak_conv_m \<mu> M"
+  shows
+    "\<exists>(\<Omega> :: real measure) (Y_seq :: nat \<Rightarrow> real \<Rightarrow> real) (Y :: real \<Rightarrow> real).
+    prob_space \<Omega> \<and>
+    (\<forall>n. Y_seq n \<in> measurable \<Omega> borel) \<and>
+    (\<forall>n. distr \<Omega> borel (Y_seq n) = \<mu> n) \<and>
+    Y \<in> measurable \<Omega> lborel \<and>
+    distr \<Omega> borel Y = M \<and>
+    (\<forall>x \<in> space \<Omega>. (\<lambda>n. Y_seq n x) \<longlonglongrightarrow> Y x)"
+text_raw \<open>}%EndSnippet\<close>
+oops
+
+text_raw \<open>\DefineSnippet{countableatoms}{\<close>
+lemma countable_atoms: "countable {x. measure M {x} > 0}"
+text_raw \<open>}%EndSnippet\<close>
+oops
+
 text_raw \<open>\DefineSnippet{clt}{\<close>
 theorem (in prob_space) central_limit_theorem:
   fixes X :: "nat \<Rightarrow> 'a \<Rightarrow> real"
@@ -192,10 +193,9 @@ oops
 text_raw \<open>\DefineSnippet{intervalmeasure}{\<close>
 lemma real_distribution_interval_measure:
   fixes F :: "real \<Rightarrow> real"
-  assumes nondecF : "\<And> x y. x \<le> y \<Longrightarrow> F x \<le> F y"
-    and right_cont_F : "\<And>a. continuous (at_right a) F"
-    and lim_F_at_bot : "(F \<longlongrightarrow> 0) at_bot"
-    and lim_F_at_top : "(F \<longlongrightarrow> 1) at_top"
+  assumes "\<And> x y. x \<le> y \<Longrightarrow> F x \<le> F y"
+    and "\<And>a. continuous (at_right a) F"
+    and "(F \<longlongrightarrow> 0) at_bot" "(F \<longlongrightarrow> 1) at_top"
   shows "real_distribution (interval_measure F)"
 text_raw \<open>}%EndSnippet\<close>
 oops
@@ -204,10 +204,215 @@ text_raw \<open>\DefineSnippet{cdfunique}{\<close>
 lemma cdf_unique:
   fixes M1 M2
   assumes "real_distribution M1" and "real_distribution M2"
-  assumes "cdf M1 = cdf M2"
+    and "cdf M1 = cdf M2"
   shows "M1 = M2"
 text_raw \<open>}%EndSnippet\<close>
 oops
+
+text_raw \<open>\DefineSnippet{lebesgueintegralcountableadd}{\<close>
+lemma lebesgue_integral_countable_add:
+  fixes f :: "_ \<Rightarrow> 'a :: {banach, second_countable_topology}"
+  assumes meas[intro]: "\<And>i::nat. A i \<in> sets M"
+    and disj: "\<And>i j. i \<noteq> j \<Longrightarrow> A i \<inter> A j = {}"
+    and intgbl: "set_integrable M (\<Union>i. A i) f"
+  shows "LINT x:(\<Union>i. A i)|M. f x = (\<Sum>i. (LINT x:(A i)|M. f x))"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{dominatedconvergence}{\<close>
+lemma dominated_convergence:
+  fixes f :: "'a \<Rightarrow> 'b::{banach, second_countable_topology}"
+    and w :: "'a \<Rightarrow> real"
+  assumes "f \<in> borel_measurable M"
+    and "\<And>i. s i \<in> borel_measurable M" "integrable M w"
+    and "AE x in M. (\<lambda>i. s i x) \<longlonglongrightarrow> f x"
+    and "\<And>i. AE x in M. norm (s i x) \<le> w x"
+  shows "integrable M f" and "\<And>i. integrable M (s i)"
+    and "(\<lambda>i. integral\<^sup>L M (s i)) \<longlonglongrightarrow> integral\<^sup>L M f"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{integralnormbound}{\<close>
+lemma integral_norm_bound:
+  fixes f :: "_ \<Rightarrow> 'a :: {banach, second_countable_topology}"
+  shows "integrable M f \<Longrightarrow> norm (integral\<^sup>L M f) \<le> (\<integral>x. norm (f x) \<partial>M)"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{ftcfinite}{\<close>
+lemma interval_integral_FTC_finite:
+  fixes f F :: "real \<Rightarrow> 'a::euclidean_space" and a b :: real
+  assumes f: "continuous_on {min a b..max a b} f"
+   and F: "\<And>x. min a b \<le> x \<Longrightarrow> x \<le> max a b \<Longrightarrow>
+    (F has_vector_derivative (f x)) (at x within {min a b..max a b})"
+  shows "(LBINT x=a..b. f x) = F b - F a"
+text_raw \<open>}%EndSnippet\<close>
+oops
+
+text_raw \<open>\DefineSnippet{ftcintegrable}{\<close>
+lemma interval_integral_FTC_integrable:
+  fixes f F :: "real \<Rightarrow> 'a::euclidean_space" and a b :: ereal
+  assumes "a < b"
+    and "\<And>x. a < ereal x \<Longrightarrow> ereal x < b \<Longrightarrow>
+    (F has_vector_derivative f x) (at x)"
+    and "\<And>x. a < ereal x \<Longrightarrow> ereal x < b \<Longrightarrow> isCont f x"
+    and "set_integrable lborel (einterval a b) f"
+    and "((F \<circ> real_of_ereal) \<longlongrightarrow> A) (at_right a)"
+    and "((F \<circ> real_of_ereal) \<longlongrightarrow> B) (at_left b)"
+  shows "(LBINT x=a..b. f x) = B - A"
+text_raw \<open>}%EndSnippet\<close>
+oops
+
+text_raw \<open>\DefineSnippet{ftc2}{\<close>
+lemma interval_integral_FTC2:
+  fixes a b c x :: real and f :: "real \<Rightarrow> 'a::euclidean_space"
+  assumes "a \<le> c" "c \<le> b" "continuous_on {a..b} f" "a \<le> x" "x \<le> b"
+  shows "((\<lambda>u. LBINT y=c..u. f y) has_vector_derivative (f x))
+    (at x within {a..b})"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{substfinite}{\<close>
+lemma interval_integral_substitution_finite:
+  fixes a b :: real and f :: "real \<Rightarrow> 'a::euclidean_space"
+  assumes "a \<le> b" and "\<And>x. a \<le> x \<Longrightarrow> x \<le> b \<Longrightarrow>
+      (g has_real_derivative (g' x)) (at x within {a..b})"
+    and "continuous_on (g ` {a..b}) f" "continuous_on {a..b} g'"
+  shows "LBINT x=a..b. g' x *\<^sub>R f (g x) = LBINT y=g a..g b. f y"
+text_raw \<open>}%EndSnippet\<close>
+oops
+
+text_raw \<open>\DefineSnippet{substintegrable}{\<close>
+lemma interval_integral_substitution_integrable:
+  fixes f :: "real \<Rightarrow> 'a::euclidean_space" and a b u v :: ereal
+  assumes "a < b"
+    and "\<And>x. a < ereal x \<Longrightarrow> ereal x < b \<Longrightarrow> DERIV g x :> g' x"
+    and "\<And>x. a < ereal x \<Longrightarrow> ereal x < b \<Longrightarrow> isCont f (g x)"
+    and "\<And>x. a < ereal x \<Longrightarrow> ereal x < b \<Longrightarrow> isCont g' x"
+    and "\<And>x. a \<le> ereal x \<Longrightarrow> ereal x \<le> b \<Longrightarrow> 0 \<le> g' x"
+    and "((ereal \<circ> g \<circ> real_of_ereal) \<longlongrightarrow> A) (at_right a)"
+    and "((ereal \<circ> g \<circ> real_of_ereal) \<longlongrightarrow> B) (at_left b)"
+    and "set_integrable lborel (einterval a b) (\<lambda>x. g' x *\<^sub>R f (g x))"
+    and "set_integrable lborel (einterval A B) (\<lambda>x. f x)"
+  shows "(LBINT x=A..B. f x) = (LBINT x=a..b. g' x *\<^sub>R f (g x))"
+text_raw \<open>}%EndSnippet\<close>
+oops
+
+text_raw \<open>\DefineSnippet{charprop1}{\<close>
+lemma (in real_distribution) isCont_char: "isCont (char M) t"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{charprop2}{\<close>
+lemma (in real_distribution) char_zero: "char M 0 = 1"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{charprop3}{\<close>
+lemma (in real_distribution) cmod_char_le_1: "norm (char M t) \<le> 1"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{chardistrsum}{\<close>
+lemma (in prob_space) char_distr_sum:
+  fixes X1 X2 :: "'a \<Rightarrow> real" and t :: real
+  assumes "indep_var borel X1 borel X2"
+  shows "char (distr M borel (\<lambda>\<omega>. X1 \<omega> + X2 \<omega>)) t =
+    char (distr M borel X1) t * char (distr M borel X2) t"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{chardistrsetsum}{\<close>
+lemma (in prob_space) char_distr_setsum:
+  "indep_vars (\<lambda>i. borel) X A \<Longrightarrow>
+    char (distr M borel (\<lambda>\<omega>. \<Sum>i\<in>A. X i \<omega>)) t =
+    (\<Prod>i\<in>A. char (distr M borel (X i)) t)"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{charapprox}{\<close>
+lemma (in prob_space) char_approx3':
+  fixes \<mu> :: "real measure" and X
+  assumes "random_variable borel X"
+    and "integrable M X" "integrable M (\<lambda>x. (X x)^2)" "expectation X = 0"
+    and "variance X = \<sigma>2"
+    and "\<mu> = distr M borel X"
+  shows "cmod (char \<mu> t - (1 - t^2 * \<sigma>2 / 2)) \<le>
+    (t^2 / 6) * expectation (\<lambda>x. min (6 * (X x)^2) (\<bar>t\<bar> * \<bar>X x\<bar>^3))"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{momenteven}{\<close>
+lemma std_normal_moment_even:
+  "has_bochner_integral lborel (\<lambda>x. std_normal_density x * x ^ (2 * k))
+    (fact (2 * k) / (2^k * fact k))"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{momentodd}{\<close>
+lemma std_normal_moment_odd:
+  "has_bochner_integral lborel (\<lambda>x. std_normal_density x * x^(2 * k + 1)) 0"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{levyunique}{\<close>
+theorem Levy_uniqueness:
+  fixes M1 M2 :: "real measure"
+  assumes "real_distribution M1" "real_distribution M2"
+    and "char M1 = char M2"
+  shows "M1 = M2"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{levycont}{\<close>
+theorem levy_continuity:
+  fixes M :: "nat \<Rightarrow> real measure" and M' :: "real measure"
+  assumes "\<And>n. real_distribution (M n)"
+    and "real_distribution M'"
+    and "\<And>t. (\<lambda>n. char (M n) t) \<longlonglongrightarrow> char M' t"
+  shows "weak_conv_m M M'"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{levycont1}{\<close>
+theorem levy_continuity1:
+  "(\<And>n. real_distribution (M n)) \<Longrightarrow> real_distribution M' \<Longrightarrow>
+    weak_conv_m M M' \<Longrightarrow>
+    (\<lambda>n. char (M n) t) \<longlonglongrightarrow> char M' t"
+  unfolding char_def by (rule weak_conv_imp_integral_bdd_continuous_conv) auto
+text_raw \<open>}%EndSnippet\<close>
+
+text_raw \<open>\DefineSnippet{helly}{\<close>
+theorem Helly_selection:
+  fixes f :: "nat \<Rightarrow> real \<Rightarrow> real"
+  assumes "\<And>n x. continuous (at_right x) (f n)"
+    and "\<And>n. mono (f n)"
+    and "\<And>n x. \<bar>f n x\<bar> \<le> M"
+  shows "\<exists>s. subseq s \<and> mono F \<and> (\<forall>x. \<bar>F x\<bar> \<le> M) \<and>
+    (\<exists>F. (\<forall>x. continuous (at_right x) F) \<and>
+    (\<forall>x. continuous (at x) F \<longrightarrow> (\<lambda>n. f (s n) x) \<longlonglongrightarrow> F x))"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{thight}{\<close>
+theorem tight_imp_convergent_subsubsequence:
+  assumes \<mu>: "tight \<mu>" "subseq s"
+  shows "\<exists>r M. subseq r \<and> real_distribution M \<and> weak_conv_m (\<mu> \<circ> s \<circ> r) M"
+text_raw \<open>}%EndSnippet\<close>
+oops
+
+text_raw \<open>\DefineSnippet{integraladd}{\<close>
+lemma integral_add:
+  "integrable M f \<Longrightarrow> integrable M g \<Longrightarrow>
+    integral\<^sup>L M (\<lambda>x. f x + g x) = integral\<^sup>L M f + integral\<^sup>L M g"
+text_raw \<open>}%EndSnippet\<close>
+  oops
+
+text_raw \<open>\DefineSnippet{integrableadd}{\<close>
+lemma integrable_add:
+  "integrable M f \<Longrightarrow> integrable M g \<Longrightarrow> integrable M (\<lambda>x. f x + g x)"
+text_raw \<open>}%EndSnippet\<close>
+  oops
 
 text_raw \<open>\DefineSnippet{weakconv}{\<close>
 definition
@@ -220,5 +425,88 @@ definition
 where
   "weak_conv_m M_seq M \<equiv> weak_conv (\<lambda>n. cdf (M_seq n)) (cdf M)"
 text_raw \<open>}%EndSnippet\<close>
+
+text_raw \<open>\DefineSnippet{distr}{\<close>
+definition
+  distr :: "'a measure \<Rightarrow> 'b measure \<Rightarrow> ('a \<Rightarrow> 'b) \<Rightarrow> 'b measure"
+where
+  "distr M N f =
+    measure_of (space N) (sets N) (\<lambda>A. emeasure M (f -` A \<inter> space M))"
+text_raw \<open>}%EndSnippet\<close>
+
+text_raw \<open>\DefineSnippet{density}{\<close>
+definition
+  density :: "'a measure \<Rightarrow> ('a \<Rightarrow> ennreal) \<Rightarrow> 'a measure"
+where
+  "density M f =
+    measure_of (space M) (sets M) (\<lambda>A. \<integral>\<^sup>+ x. f x * indicator A x \<partial>M)"
+text_raw \<open>}%EndSnippet\<close>
+
+
+
+text_raw \<open>\DefineSnippet{probspace}{\<close>
+locale prob_space =
+  fixes M :: "'a measure" assumes "emeasure M (space M) = 1"
+begin
+  abbreviation "events \<equiv> sets M"
+  abbreviation "prob \<equiv> measure M"
+  abbreviation "random_variable M' X \<equiv> X \<in> measurable M M'"
+  abbreviation "expectation \<equiv> integral\<^sup>L M"
+  abbreviation "variance X \<equiv> integral\<^sup>L M (\<lambda>x. (X x - expectation X)\<^sup>2)"
+end
+text_raw \<open>}%EndSnippet\<close>
+
+
+text_raw \<open>\DefineSnippet{indepsets}{\<close>
+definition (in prob_space)
+  indep_sets :: "('i \<Rightarrow> 'a set set) \<Rightarrow> 'i set \<Rightarrow> bool"
+where
+  "indep_sets F I \<longleftrightarrow>
+    (\<forall>i\<in>I. F i \<subseteq> events) \<and>
+    (\<forall>J\<subseteq>I. J \<noteq> {} \<longrightarrow> finite J \<longrightarrow>
+      (\<forall>A\<in>(\<Pi> i\<in>J. F i). prob (\<Inter>j\<in>J. A j) = (\<Prod>j\<in>J. prob (A j))))"
+text_raw \<open>}%EndSnippet\<close>
+
+text_raw \<open>\DefineSnippet{indepevents}{\<close>
+definition (in prob_space) "indep_events A I \<longleftrightarrow> indep_sets (\<lambda>i. {A i}) I"
+text_raw \<open>}%EndSnippet\<close>
+
+text_raw \<open>\DefineSnippet{indepvars}{\<close>
+definition (in prob_space)
+  indep_vars :: "('i \<Rightarrow> 'b measure) \<Rightarrow> ('i \<Rightarrow> 'a \<Rightarrow> 'b) \<Rightarrow> 'i set \<Rightarrow> bool"
+where
+  "indep_vars M' X I \<longleftrightarrow>
+    (\<forall>i\<in>I. random_variable (M' i) (X i)) \<and>
+    indep_sets (\<lambda>i. { X i -` A \<inter> space M | A. A \<in> sets (M' i)}) I"
+text_raw \<open>}%EndSnippet\<close>
+
+text_raw \<open>\DefineSnippet{char}{\<close>
+definition
+  char :: "real measure \<Rightarrow> real \<Rightarrow> complex"
+where
+  "char M t = CLINT x|M. iexp (t * x)"
+text_raw \<open>}%EndSnippet\<close>
+
+experiment
+begin
+
+text_raw \<open>\DefineSnippet{normaldistr}{\<close>
+definition
+  normal_density :: "real \<Rightarrow> real \<Rightarrow> real \<Rightarrow> real"
+where
+  "normal_density \<mu> \<sigma> x = 1 / sqrt (2 * pi * \<sigma>\<^sup>2) * exp (-(x - \<mu>)\<^sup>2/ (2 * \<sigma>\<^sup>2))"
+
+abbreviation
+  std_normal_density :: "real \<Rightarrow> real"
+where
+  "std_normal_density \<equiv> normal_density 0 1"
+
+abbreviation
+  std_normal_distribution :: "real measure"
+where
+  "std_normal_distribution \<equiv> density lborel std_normal_density"
+text_raw \<open>}%EndSnippet\<close>
+
+end
 
 end
